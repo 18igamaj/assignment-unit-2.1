a5f2de8cac3220026ad3231466b81e5ff778c9e2
Majid Igale <majidigale18@gmail.com>
2023-04-23 08:51:27 -0500

    fixed my mistakes and ready for resubmission

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -1,12 +1,12 @@
 // REQUIRED FEATURES:
 console.log('js')
 // 1 - Create a variable called `firstName` and assign it the value of your first name
-let firstName = 'Majid ';
+let firstName = 'Majid';
 // 2 - Create a second variable called `lastName` and assign it the value of your last name
 let lastName = 'Igale ';
 // 3 - Create a third variable called `fullName`, assign it the value of your first and last name
 // (remember, you already have variables for this, can you use those?)
-let fullName = firstName + lastName;
+let fullName = firstName + ' ' + lastName;
 
 // 4 - Console log the value of `fullName`
 console.log(fullName);
@@ -42,7 +42,7 @@ else{ console.log('How about we stay home?')}
 
 // 14 - Create a compound conditional: if luckyNumber is 2 and adventurous is true,
 // console log "Roll the dice!"
-if( luckyNumber >= 2 & adventurous){
+if( luckyNumber = 2 && adventurous){
 console.log('Roll the dice')}
 
 else{
